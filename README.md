3eee63c90899977152469bc737f5ddc465664d32
xLsDg <xlsdg@qq.com>
2017-01-13 15:31:43 +0800

    fix(file): Change the file name to lowercase

---
+++ README.md
@@ -0,0 +1,5 @@
+# dva-antd-starter
+
+> Get started with Dva.js and Ant Design.
+
+![Screen Shot](screenshot.png)
---
+++ src/components/layout/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './footer.css';
+
+function Footer() {
+  return (
+    <footer className={styles.normal}>
+      Ant Design ©2016 Created by Ant UED
+    </footer>
+  );
+}
+
+export default Footer;
