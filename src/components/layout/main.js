146d2f447b12686b0c81f5fccd935a2683a44e68
xLsDg <xlsdg@qq.com>
2017-04-11 11:09:47 +0800

    fix(mod): add prop-types module

--- src/components/layout/main.js
+++ src/components/layout/main.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {Layout, BackTop} from 'antd';
 
 import Header from './header';
--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {Router, Route} from 'dva/router';
 
 import Index from './routes/index.js';
