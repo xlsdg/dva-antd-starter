b4cd4667cd3fddb000d8ea43571003feac376a0a
xLsDg <xlsdg@qq.com>
2017-04-26 17:09:00 +0800

    chore(roadhog): add publicPath

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -1,18 +1,19 @@
 export default {
-  'entry': 'src/index.js',
-  'disableCSSModules': false,
-  'autoprefixer': null,
-  'proxy': null,
-  'extraBabelPlugins': [
+  entry: 'src/index.js',
+  publicPath: '/dva-antd-starter/',
+  disableCSSModules: false,
+  autoprefixer: null,
+  proxy: null,
+  extraBabelPlugins: [
     'transform-runtime',
     ['import', {
-      'libraryName': 'antd',
-      'style': true
+      libraryName: 'antd',
+      style: true
     }]
   ],
-  'env': {
-    'development': {
-      'extraBabelPlugins': [
+  env: {
+    development: {
+      extraBabelPlugins: [
         'dva-hmr'
       ]
     }
