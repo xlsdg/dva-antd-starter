fae83e7eedc409eb23d8d17475d8edfbcc42e100
xLsDg <xlsdg@qq.com>
2017-09-05 11:02:45 +0800

    chore(mod): upgrade modules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import 'babel-polyfill';
 import dva from 'dva';
 import Loading from 'dva-loading';
+import {
+  message
+} from 'antd';
 
 import './index.less';
 
 // 1. Initialize
-const app = dva();
+const app = dva({
+  onError(err, dispatch) {
+    message.error(err);
+  }
+});
 
 // 2. Plugins
 app.use(Loading({
@@ -14,7 +21,7 @@ app.use(Loading({
 }));
 
 // 3. Model
-// app.model(require('./models/example'));
+// Moved to router.js
 
 // 4. Router
 app.router(require('./router.jsx'));
