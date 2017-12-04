fa32e3acbf2558ddb91e07b9ff4148d577bba027
xLsDg <xlsdg@qq.com>
2017-12-04 15:52:52 +0800

    chore(mod): upgrade modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill';
 import dva from 'dva';
-import Loading from 'dva-loading';
+// import Loading from 'dva-loading';
 import {
   message
 } from 'antd';
@@ -10,15 +10,21 @@ import './index.less';
 // 1. Initialize
 const app = dva({
   onError(err, dispatch) {
-    message.error(err);
+    if (err.resp) {
+      message.error(err.resp.msg);
+    } else if (err.srv) {
+      message.error(err.srv.msg);
+    } else {
+      message.error(err);
+    }
   }
 });
 
 // 2. Plugins
-app.use(Loading({
-  namespace: 'loading'
-  // effects: enable effects level loading state
-}));
+// app.use(Loading({
+//   namespace: 'loading'
+//   // effects: enable effects level loading state
+// }));
 
 // 3. Model
 // Moved to router.js
